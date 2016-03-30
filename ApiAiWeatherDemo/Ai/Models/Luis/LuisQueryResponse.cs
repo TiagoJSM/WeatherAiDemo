f1d155194bf6339d6b52e822cd9ad92ab530a4fa
Jack Graham <jgraham@scottlogic.co.uk>
2016-03-30 14:41:26 +0100

    Added Luis ai service

---
+++ ApiAiWeatherDemo/Ai/Models/Luis/LuisQueryResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiAiWeatherDemo.Ai.Models.Luis
+{
+    public class LuisQueryResponse
+    {
+        public String query { get; set; }
+        public Intent[] intents { get; set; }
+        public Entity[] entities { get; set; }
+    }
+
+    public class Entity
+    {
+        public String entity { get; set; }
+        public String type { get; set; }
+        public int startIndex { get; set; }
+        public int endIndex { get; set; }
+        public long score { get; set; }
+    }
+
+    public class Intent
+    {
+        public String intent { get; set; }
+        public long score { get; set; }
+        public String action { get; set; }
+    }
+}
