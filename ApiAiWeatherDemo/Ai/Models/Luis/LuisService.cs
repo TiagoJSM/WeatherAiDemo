9f1eea0cc464d9c029b7aa2aecf20346f9f19afb
Jack Graham <jgraham@scottlogic.co.uk>
2016-03-31 10:31:13 +0100

    Fixed weather api to return it in celsius, now taking confidence into concideration in luis service

--- ApiAiWeatherDemo/Ai/Models/Luis/LuisService.cs
+++ ApiAiWeatherDemo/Ai/Models/Luis/LuisService.cs
@@ -14,7 +14,8 @@ namespace ApiAiWeatherDemo.Ai
         {
             query = HttpUtility.HtmlEncode(query);
             var client = new RestClient("https://api.projectoxford.ai/luis/v1/");
-            var request = new RestRequest(@"application?id=858a25c8-f21f-47fd-8b1f-80b3a051b1d5&subscription-key=97c75fab726c47f685fb964878f5fac4&q={query}", Method.GET);
+            var request = new RestRequest(@"application?id=858a25c8-f21f-47fd-8b1f-80b3a051b1d5&subscription-key={key}&q={query}", Method.GET);
+            request.AddUrlSegment("key", "97c75fab726c47f685fb964878f5fac4");
             request.AddUrlSegment("query", query);
 
             var response = client.Execute<LuisQueryResponse>(request);
@@ -22,7 +23,17 @@ namespace ApiAiWeatherDemo.Ai
 
             LuisQueryResponse res = Newtonsoft.Json.JsonConvert.DeserializeObject<LuisQueryResponse>(content);
 
-            if (res.intents[0].intent != "getWeather")
+
+            Intent intent = null;
+            intent = res.intents.FirstOrDefault(item => item.intent == "getWeather");
+
+            if (intent == null)
+            {
+                return null;
+            }
+
+            //If the confidence is too low we return null as the ai might have returned a false intent
+            if(res.intents[0].score < 0.75)
             {
                 return null;
             }
