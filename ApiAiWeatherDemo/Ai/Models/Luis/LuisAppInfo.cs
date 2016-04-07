4e7fdeb32e6d9b927bb3e6e340fc360373fabcef
Jack Graham <jgraham@scottlogic.co.uk>
2016-04-07 13:54:04 +0100

    Added service method to get luis app info

---
+++ ApiAiWeatherDemo/Ai/Models/Luis/LuisAppInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiAiWeatherDemo.Ai.Models.Luis
+{
+    public class LuisAppInfo
+    {
+        public String ID { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public String Culture { get; set; }
+        public Boolean Active { get; set; }
+        public String CreatedDate { get; set; }
+        public String ModifiedDate { get; set; }
+        public String PublishDate { get; set; }
+        public String URL { get; set; }
+        public String AuthKey { get; set; }
+        public int NumberOfIntents { get; set; }
+        public int NumberOfEntities { get; set; }
+        public Boolean IsTrained { get; set; }
+    }
+}
